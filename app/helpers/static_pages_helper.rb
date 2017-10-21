55dbd78b4c7401d44ac921b05c62605819e467e4
NicholasJacques <nicholas.d.jacques@gmail.com>
2017-10-21 12:36:52 -0600

    Sign in restaurant

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -3,6 +3,8 @@ module StaticPagesHelper
     if current_user
       if current_user.contractor?
         render 'layouts/contractor_header'
+      else 
+        render 'layouts/restaurant_header'
       end
     else
       render 'layouts/guest_header'
